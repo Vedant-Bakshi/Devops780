077084251924d6555c9833c3056f5f2715fde5da
vedantbakshi <bakshi.vedant@gmail.com>
2024-11-19 20:19:04 +0530

    vpc has corrected

--- mynewenv.tf
+++ mynewenv.tf
@@ -1,1 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
 
+  tags = {
+    Name = "Myvpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.yvpc.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
